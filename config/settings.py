c38be5c1a5122d26fe5491d71495e6ddf8d9a17a
Vitaly Shcheglov <hallovit@yandex.ru>
2025-08-20 11:06:19 +0500

    correction2

--- config/settings.py
+++ config/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = os.getenv("SECRET_KEY", "django-insecure-_9qvgo$$__op2s%=9g7h#*4ig5
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG", "False") == "True"
 
-ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS").split(",")
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "localhost,127.0.0.1").split(",")
 
 
 # Application definition
